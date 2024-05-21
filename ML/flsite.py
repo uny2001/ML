bdecf30016d3923272f36cfbb222e889905b36a3
uny2001 <uny2001@yandex.ru>
2024-05-21 08:36:59 +0300

    Сделал API

--- ML/flsite.py
+++ ML/flsite.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 from sklearn.preprocessing import LabelEncoder
 label_encoder=LabelEncoder()
 
@@ -46,6 +46,27 @@ def f_lab1():
         return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu,
                                class_model="Это: " + sort)
 
+@app.route('/api', methods=['get'])
+def get_sort():
+    X_new = np.array([[float(request.args.get('kkal')),
+                       float(request.args.get('width')),
+                       float(request.args.get('sugar'))]])
+    pred = loaded_model_knn.predict(X_new)
+    print(pred[0])
+    if pred == "[0]":
+       sort = "Kalhoznitsa"
+    elif pred == 1:
+        sort = "Torpeda"
+    elif pred == "[2]":
+        sort = "Pyatiminutka"
+    elif pred == "[3]":
+        sort = "Alyasks"
+    elif pred == "[4]":
+        sort = "Sangria"
+    else:
+        sort = 'defult'
+    return jsonify(sort=sort) # http://127.0.0.1:5000/api?kkal=25&width=25&sugar=3
+
 @app.route("/p_lab2", methods=['POST', 'GET'])
 def f_lab2():
     if request.method == 'GET':
@@ -54,7 +75,7 @@ def f_lab2():
         X_new = np.array([[int(request.form['list1']),
                            int(request.form['list2']),
                            int(request.form['list3'])]])
-        pred = str(loaded_model_Log.predict(X_new))
+        pred = loaded_model_Log.predict(X_new)
         if pred == "[0]":
             sort = "Калхозница"
         elif pred == "[1]":
